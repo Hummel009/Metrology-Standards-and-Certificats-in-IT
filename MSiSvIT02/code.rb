fc2dc964b89201758edccaacb57a37a9efe03187
Hummel009 <60618853+Hummel009@users.noreply.github.com>
2023-04-29 11:14:13 +0300

    1

--- MSiSvIT02/code.rb
+++ MSiSvIT02/code.rb
@@ -1,76 +1,37 @@
 begin
-a = 10
-b = 20.3
-c = 15
-a = a + 30
-sum = a + b * c - 100 + a*b*c
-if(a<b)
-	if(c==0)
-		del = b
-	end
- 	b = c
-else
-	if(a==b)
-		b = c
-		c = a
-	else
-		c = a
-		a = b
-	end
-end
+	x = 0
+	y = 0
 
-if (a==b)
-	a = a*100
- 	b = 200
-end
+	puts x
 
-while(a<b) do
-while(a<b) do
- a = a + 10
- a = b + 1000
-end
- a = b + 1000
-end
-
-loop do
- c = c * b + 100
-	if( c > 10000)
-		break
-	end
-	if( c < 100)
-		break
-	end
-end
-
-until ( c == 5 ) do
-   c = c + 1;
-   puts c = c + 2
-end
-
-
-puts sum * a / b
-
-case a
-when 10
-	a = 100
-when 20
-	a = 100321
-		while (a < 1000) do
-			a = a + 100
+	while (x == 0) do 
+		while (x == 1) do 
+			while (x == 2) do 
+				while (x == 3) do 
+					while (x == 4) do 
+						if (x == 0)
+							y = 0
+						else
+							if (x == 1)
+								y = 1
+							else
+								if (x == 2)
+									y = 2
+								else
+									if (x == 3)
+										y = 3
+									else
+										y = 4
+									end
+								end
+							end
+						end
+					end
+				end
+			end
 		end
-when 590
-	a = 100342
-when 30
-		a = a + 30
-		while (a < 1000) do
-			a = a + 100
-		end
-		puts a + b
-else
-	puts a
-	puts b
-end
-
-puts aboba
+	end
+	
+	puts y
 end
 
