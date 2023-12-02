a6fa0212756859dd01e6a4dd88c7e026518f1e2e
Hummel009 <hummel.turbamentum@gmail.com>
2023-12-02 17:59:52 +0300

    Java 21, Kotlin 1.9.21, Gradle 8.5

--- Lab03/build.gradle.kts
+++ Lab03/build.gradle.kts
@@ -2,7 +2,7 @@ import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
 plugins {
-	id("org.jetbrains.kotlin.jvm") version "1.9.20"
+	id("org.jetbrains.kotlin.jvm") version "1.9.21"
 	id("application")
 }
 
@@ -16,7 +16,7 @@ repositories {
 val embed: Configuration by configurations.creating
 
 dependencies {
-	embed("org.jetbrains.kotlin:kotlin-stdlib:1.9.20")
+	embed("org.jetbrains.kotlin:kotlin-stdlib:1.9.21")
 	embed("com.formdev:flatlaf:3.2.1")
 	embed("com.formdev:flatlaf-intellij-themes:3.2.1")
 	implementation("com.formdev:flatlaf:3.2.1")
@@ -25,7 +25,7 @@ dependencies {
 
 java {
 	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
+		languageVersion = JavaLanguageVersion.of(21)
 	}
 }
 
