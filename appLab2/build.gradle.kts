828241d4de6d54c943b797eaf5c08eb46941d367
Hummel009 <hummel.turbamentum@gmail.com>
2024-01-14 17:19:29 +0300

    kotlin 1.9.22

--- appLab2/build.gradle.kts
+++ appLab2/build.gradle.kts
@@ -12,7 +12,7 @@ version = "v" + LocalDate.now().format(DateTimeFormatter.ofPattern("yy.MM.dd"))
 val embed: Configuration by configurations.creating
 
 dependencies {
-	embed("org.jetbrains.kotlin:kotlin-stdlib:1.9.21")
+	embed("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
 	embed("com.formdev:flatlaf:3.2.5")
 	embed("com.formdev:flatlaf-intellij-themes:3.2.5")
 	implementation("com.formdev:flatlaf:3.2.5")
